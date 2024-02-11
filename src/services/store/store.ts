a4f14fdd9088a893e38ce4352a039e399855cfce
Artostapyshyn <artostapyshyn@gmail.com>
2024-02-11 18:37:26 +0200

    store fixes

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -5,14 +5,12 @@ import {
 } from "redux";
 import {client} from "../reducers/clientReducer.tsx";
 import thunk from "redux-thunk";
-// @ts-ignore
-import {composeWithDevTools} from "@reduxjs/toolkit/dist/devtoolsExtension";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 export const combineReducer = combineReducers({
     client,
 })
 
-// @ts-ignore
 export const store = createStore(
     combineReducer,
     composeWithDevTools(applyMiddleware(thunk))
